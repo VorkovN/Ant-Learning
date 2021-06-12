1455c9c33a6b186f9b13f0993270ea4ffc200f15
VorkovN <vorkov711@gmail.com>
2021-06-12 16:02:15 +0300

    7

--- hello/src/HelloWorld.java
+++ hello/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println(test1());
-        System.out.println(test2());
+        System.out.printlsdccsn(test2());
         System.out.println(test3());
     }
 
